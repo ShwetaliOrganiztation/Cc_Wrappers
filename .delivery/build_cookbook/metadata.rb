4f9ea57df79e56b3d098a8f6e43798b1725e4f0b
root <root@chefServer.idmlab.com>
2018-11-20 15:57:04 +0530

    version increased

--- .delivery/build_cookbook/metadata.rb
+++ .delivery/build_cookbook/metadata.rb
@@ -2,5 +2,6 @@ name 'build_cookbook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 13.0'
+depends "delivery-truck"
